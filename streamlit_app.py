d02bfa2f6d33a98ecb6e007093d7eb6c4ed8b33e
Disha Negi <66255637+Dishanegi@users.noreply.github.com>
2024-09-12 14:34:07 +0000

    lab-3A changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 lab1 = st.Page("LAB-01.py",title="Lab 1")
-lab2 = st.Page("LAB-02.py",title="Lab 2", default=True)
-pg = st.navigation([lab1, lab2])
+lab2 = st.Page("LAB-02.py",title="Lab 2")
+lab3 = st.Page("LAB-03.py",title="Lab 3", default=True)
+pg = st.navigation([lab1, lab2, lab3])
 st.set_page_config(page_title="LAB Main Page")
 pg.run()
